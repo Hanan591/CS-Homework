df62b7f2c7b840e43099e3918cc576156dbb910c
Hanan591 <151069406+Hanan591@users.noreply.github.com>
2025-02-07 23:17:08 -0500

    Delete Inheritance directory

--- Inheritance/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        NonPlayerCharacter npc = new NonPlayerCharacter();
-        System.out.println(npc.reportStructure());
-       // System.out.println(npc.introduce());
-        //System.out.println(npc.exclaim());
-    }
-}
